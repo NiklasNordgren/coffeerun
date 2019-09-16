27c06a54ab34b2ddada4637b1f20fe8f9d071b5b
NiklasNordgren <39832486+NiklasNordgren@users.noreply.github.com>
2019-09-16 15:15:12 +0200

    added formhandler

---
+++ scripts/formhandler.js
@@ -0,0 +1,12 @@
+(function(window) {
+  'use strict';
+  var App = window.App || {};
+  var $ = window.jQuery;
+
+  function FormHandler() {
+    // Code will go here
+  }
+
+  App.FormHandler = FormHandler;
+  window.App = App;
+})(window);
