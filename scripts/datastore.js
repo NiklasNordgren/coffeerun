546c62d8090331ba28efbfde8d7138aa12f62699
NiklasNordgren <39832486+NiklasNordgren@users.noreply.github.com>
2019-09-16 12:41:38 +0200

    Silver Challenge: Making data Private

--- scripts/datastore.js
+++ scripts/datastore.js
@@ -1,6 +1,13 @@
 (function(window) {
   'use strict';
   var App = window.App || {};
+  var data = {};
+
+  /*
+    Silver Challenge: Making data Private
+    Declaration of the property data in the outside scope.
+    All instances of DataStore will now share the same property data.
+  */
 
   function DataStore() {
     this.data = {};
--- scripts/main.js
+++ scripts/main.js
@@ -3,4 +3,6 @@
   var App = window.App;
   var Truck = App.Truck;
   var DataStore = App.DataStore;
+  var myTruck = new Truck('KITT', new DataStore());
+  window.myTruck = myTruck;
 })(window);
