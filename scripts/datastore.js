f8fcef30cc792ba70a1f9f1a38cc62dfd5111cb4
NiklasNordgren <39832486+NiklasNordgren@users.noreply.github.com>
2019-09-16 12:49:05 +0200

    Update datastore.js

--- scripts/datastore.js
+++ scripts/datastore.js
@@ -1,7 +1,7 @@
 (function(window) {
   'use strict';
   var App = window.App || {};
-  var data = {};
+  var data;
 
   /*
     Silver Challenge: Making data Private
@@ -10,23 +10,23 @@
   */
 
   function DataStore() {
-    this.data = {};
+    data = {};
   }
 
   DataStore.prototype.add = function(key, val) {
-    this.data[key] = val;
+    data[key] = val;
   };
 
   DataStore.prototype.get = function(key) {
-    return this.data[key];
+    return data[key];
   };
 
   DataStore.prototype.getAll = function() {
-    return this.data;
+    return data;
   };
 
   DataStore.prototype.remove = function(key) {
-    delete this.data[key];
+    delete data[key];
   };
 
   App.DataStore = DataStore;
